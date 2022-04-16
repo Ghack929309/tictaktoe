dd2feaec2618f41be450d4e2e3659b53f846e727
jonathan.calixte <jonathan.calixte@latoken.com>
2022-04-17 02:51:38 +0300

    win beta

--- script.js
+++ script.js
@@ -2,33 +2,13 @@ const canvas = document.querySelector('.canvasEl')
 const ctx = canvas.getContext('2d')
 const turn = document.querySelector('.turn')
 canvas.width = innerWidth
+let score = 0
 canvas.height= innerHeight
 const width = 60
 const height = 60
 const playerTurn = ['Your', 'Robot']
 turn.innerHTML = playerTurn[Math.floor(Math.random()* playerTurn.length)]
-/*TODO GRID 3X3 DONE
-  the grid should be clickable TODO DONE
-  the grid cells should have the correct player sign displayed an information display//TODO DONE
-  should display a message informing the current player it’s their turn//TODO DONE
-  should show us who won the game
-  should show us if the game ended in a draw
-  restart button
-    will restart the entire game
- */
 
-/* TODO BREACK DOWN
-needs to track any clicks that happen on our cells//TODO DONE
-needs to check if a valid move has been made//TODO DONE
-needs to make sure nothing happens if an already played cell has been clicked//TODO DONE
-we should update our game state
-we should validate the game state
-check if a player has won
-check if the game ended in a draw
-either stop the game or change the active player, depending on the above checks
-reflect the updates made on the UI
-rinse and repeat
- */
 class Grid{
     constructor({position,name,pressed}) {
         this.position=position
@@ -48,6 +28,7 @@ class Grid{
         ctx.fillStyle=color
         ctx.fillRect(coordinate.x,coordinate.y,10,10)
         ctx.restore()
+
     }
     drawO({coordinate,color}){
         ctx.save()
@@ -91,14 +72,15 @@ const validMove={
       boundary.draw()
       let userMove = true
       addEventListener('mousedown', (e) => {
-              if (e.clientX >= boundary.position.x &&
+
+          if (e.clientX >= boundary.position.x &&
                   e.clientX <= boundary.position.x + 60 &&
                   e.clientY >= boundary.position.y &&
-                  e.clientY <= boundary.position.y + 60 &&
-                  !boundary.pressed ) {
+                  e.clientY <= boundary.position.y + 60 ) {
                   const valid = validMove.random.some(move => move === boundary.name)
                   userMove = valid
                   if (valid) {
+                      boundary.pressed = true
                   boundary.drawX({
                       coordinate: {
                           x: boundary.position.x + (width / 2) - 5,
@@ -106,17 +88,30 @@ const validMove={
                       },
                       color: 'red'
                   })
-                  console.log(validMove.random)
+                      validMove.player.push(boundary.name)
                       validMove.random.splice(validMove.random.indexOf(boundary.name), 1)
               validMove.play=false
               turn.innerHTML='Robot'
               }
+
+          if(validMove.player.length>=3){
+              const winner = winnerFunction(validMove.player)
+              if(winner){
+                  score++
+                  document.getElementById('player').innerHTML= score
+
+              }else{
+                  validMove.player=[]
               }
+          }
+              }
+
 
           })
 
 
           addEventListener('mouseup', (e) => {
+              const winner = winnerFunction(validMove.robot)
               const randomMove = validMove.random[Math.floor(Math.random() * validMove.random.length)]
               const robotMove = boundaries.find(el => el.name === randomMove)
               const valid = validMove.random.some(move => move === robotMove.name)
@@ -134,14 +129,44 @@ const validMove={
                           y:robotMove.position.y + (height/2)-5
                       },color:'blue'
                   })
+                      validMove.robot.push(randomMove)
                   validMove.random.splice(validMove.random.indexOf(robotMove.name), 1)
                       turn.innerHTML= "Your"
                   },1000)
-                  validMove.play=true
+                  console.log(validMove.robot)
+                  if(validMove.robot.length>=3){
+
+                      if(winner){
+                          score++
+                         document.getElementById('robot').innerHTML=score
+                      }else{
+                          validMove.robot=[]
+                      }
+                  }
               }
 
           })
 
   })
+//define the winner function
+function winnerFunction (paths){
+const winnerPath = [
+    ['0-0','0-1','0-2'],
+    ['1-0','1-1','1-2'],
+    ['2-0','2-1','2-2'],
+    ['2-0','1-1','0-2'],
+    ['2-0','1-0','0-0'],
+    ['2-1','1-1','0-1'],
+    ['2-2','1-2','0-2'],
+    ['0-0','1-1','2-2'],
+
+]
+    return winnerPath.some(row => {
+    const sortedRow = row.sort()
+    const sortedPath = paths.sort()
+    return JSON.stringify(sortedPath) === JSON.stringify(sortedRow)
+
+})
+}
 
 
