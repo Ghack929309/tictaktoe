67757fdcb43931958591c9b93d1db5ba4119d3ec
jonathan.calixte <jonathan.calixte@latoken.com>
2022-04-18 03:57:48 +0300

    win function worked

--- script.js
+++ script.js
@@ -10,31 +10,37 @@ const playerTurn = ['Your', 'Robot']
 turn.innerHTML = playerTurn[Math.floor(Math.random()* playerTurn.length)]
 
 class Grid{
-    constructor({position,name,pressed}) {
+    constructor({position,name,pressed,finish}) {
         this.position=position
         this.width=width
         this.height=height
         this.name = name
         this.pressed = pressed
+        this.finish = finish
     }
 
-    draw(){
+    draw() {
         ctx.fillStyle = 'orange'
-        ctx.fillRect(this.position.x,this.position.y,this.width,this.height)
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
-    drawX({coordinate,color}){
+    drawX({coordinate, color}) {
+        if(!this.finish){
         ctx.save()
-        ctx.fillStyle=color
-        ctx.fillRect(coordinate.x,coordinate.y,10,10)
+        ctx.fillStyle = color
+        ctx.fillRect(coordinate.x, coordinate.y, 10, 10)
         ctx.restore()
+        }
 
     }
-    drawO({coordinate,color}){
-        ctx.save()
-        ctx.fillStyle=color
-        ctx.fillRect(coordinate.x,coordinate.y,10,10)
-        ctx.restore()
+
+    drawO({coordinate, color}) {
+        if(!this.finish) {
+            ctx.save()
+            ctx.fillStyle = color
+            ctx.fillRect(coordinate.x, coordinate.y, 10, 10)
+            ctx.restore()
+        }
     }
 
 }
@@ -54,7 +60,8 @@ const boundaries = []
                     x: columnIndex ===0?(innerWidth/2)-50:(innerWidth/2)-50+(62*columnIndex) ,
                     y: 62 * rowIndex===0?(innerWidth/2)-50:(innerWidth/2)-50+(62*rowIndex)
                 },name:`${rowIndex}-${columnIndex}`,
-                pressed:false
+                pressed:false,
+                finish:false
             })
         )
                 break
@@ -68,85 +75,73 @@ const validMove={
     robot:[]
 }
 
-  boundaries.forEach((boundary,index)=>{
+function startGame(e,boundary) {
+        if (e.clientX >= boundary.position.x &&
+            e.clientX <= boundary.position.x + 60 &&
+            e.clientY >= boundary.position.y &&
+            e.clientY <= boundary.position.y + 60) {
+            const valid = validMove.random.some(move => move === boundary.name)
+            if (valid) {
+                boundary.pressed = true
+                boundary.drawX({
+                    coordinate: {
+                        x: boundary.position.x + (width / 2) - 5,
+                        y: boundary.position.y + (height / 2) - 5
+                    },
+                    color: 'red'
+                })
+                validMove.player.push(boundary.name)
+                validMove.random.splice(validMove.random.indexOf(boundary.name), 1)
+                validMove.play = false
+                turn.innerHTML = 'Robot'
+                if (validMove.player.length >= 3) {
+                    if (winnerFunction(validMove.player)){
+                    document.getElementById('player').innerHTML = ++score
+                        validMove.random=[]
+                    }
+                }
+            }
+        }
+        boundary.finish =winnerFunction(validMove.player)
+}
+
+function moveRobot(e,boundary){
+        const randomMove = validMove.random[Math.floor(Math.random() * validMove.random.length)]
+        const robotMove = boundaries.find(el => el.name === randomMove)
+        const valid = validMove.random.some(move => move === robotMove.name)
+        if (e.clientX >= boundary.position.x &&
+            e.clientX <= boundary.position.x + 60 &&
+            e.clientY >= boundary.position.y &&
+            e.clientY <= boundary.position.y + 60 &&
+            valid
+        ) {
+            setTimeout(() => {
+
+                boundary.drawO({
+                    coordinate: {
+                        x: robotMove.position.x + (width / 2) - 5,
+                        y: robotMove.position.y + (height / 2) - 5
+                    }, color: 'blue'
+                })
+                validMove.robot.push(randomMove)
+                if (validMove.robot.length >= 3) {
+                    if (winnerFunction(validMove.robot)){
+                    document.getElementById('robot').innerHTML = ++score
+                        validMove.robot=[]
+                    }
+                }
+                validMove.random.splice(validMove.random.indexOf(robotMove.name), 1)
+                turn.innerHTML = "Your"
+            }, 1000)
+
+        }
+    boundary.finish =winnerFunction(validMove.player)
+}
+
+  boundaries.forEach((boundary)=>{
       boundary.draw()
-      let userMove = true
-      addEventListener('mousedown', (e) => {
-
-          if (e.clientX >= boundary.position.x &&
-                  e.clientX <= boundary.position.x + 60 &&
-                  e.clientY >= boundary.position.y &&
-                  e.clientY <= boundary.position.y + 60 ) {
-                  const valid = validMove.random.some(move => move === boundary.name)
-                  userMove = valid
-                  if (valid) {
-                      boundary.pressed = true
-                  boundary.drawX({
-                      coordinate: {
-                          x: boundary.position.x + (width / 2) - 5,
-                          y: boundary.position.y + (height / 2) - 5
-                      },
-                      color: 'red'
-                  })
-                      validMove.player.push(boundary.name)
-                      validMove.random.splice(validMove.random.indexOf(boundary.name), 1)
-              validMove.play=false
-              turn.innerHTML='Robot'
-              }
-
-          if(validMove.player.length>=3){
-              const winner = winnerFunction(validMove.player)
-              if(winner){
-                  score++
-                  document.getElementById('player').innerHTML= score
-
-              }else{
-                  validMove.player=[]
-              }
-          }
-              }
-
-
-          })
-
-
-          addEventListener('mouseup', (e) => {
-              const winner = winnerFunction(validMove.robot)
-              const randomMove = validMove.random[Math.floor(Math.random() * validMove.random.length)]
-              const robotMove = boundaries.find(el => el.name === randomMove)
-              const valid = validMove.random.some(move => move === robotMove.name)
-              if (e.clientX >= boundary.position.x &&
-                  e.clientX <= boundary.position.x + 60 &&
-                  e.clientY >= boundary.position.y &&
-                  e.clientY <= boundary.position.y + 60 &&
-                  valid && userMove
-                    ) {
-                  setTimeout(()=>{
-
-                  boundary.drawO({
-                      coordinate:{
-                          x:robotMove.position.x + (width/2)-5,
-                          y:robotMove.position.y + (height/2)-5
-                      },color:'blue'
-                  })
-                      validMove.robot.push(randomMove)
-                  validMove.random.splice(validMove.random.indexOf(robotMove.name), 1)
-                      turn.innerHTML= "Your"
-                  },1000)
-                  console.log(validMove.robot)
-                  if(validMove.robot.length>=3){
-
-                      if(winner){
-                          score++
-                         document.getElementById('robot').innerHTML=score
-                      }else{
-                          validMove.robot=[]
-                      }
-                  }
-              }
-
-          })
-
+      addEventListener('mousedown', (e) => startGame(e,boundary))
+      addEventListener('mouseup', (e) => moveRobot(e,boundary))
   })
 //define the winner function
 function winnerFunction (paths){
@@ -161,12 +156,28 @@ const winnerPath = [
     ['0-0','1-1','2-2'],
 
 ]
-    return winnerPath.some(row => {
-    const sortedRow = row.sort()
-    const sortedPath = paths.sort()
-    return JSON.stringify(sortedPath) === JSON.stringify(sortedRow)
-
-})
+let result = false
+    winnerPath.some(winner=>{
+       const sortWinner = winner.sort()
+       const sortPath = paths.sort()
+      if(JSON.stringify(sortPath)===JSON.stringify(sortWinner)){
+          return result=true
+      }else{
+         const tracker =[]
+         return sortWinner.forEach((win)=>{
+              sortPath.forEach((path)=>{
+                  if(win===path){
+                      tracker.push(path)
+                      if(tracker.length>2){
+                          return result = true
+                      }
+                  }
+              })
+          })
+      }
+    })
+    return result
 }
+
 
 
